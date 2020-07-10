16b523a54af0627f12ec7c6491fbad37cd10064e
jatinverma12 <jatinverma4621@gmail.com>
2020-07-10 15:51:02 +0530

    SinglePageApplication Part2;

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle,Breadcrumb,BreadcrumbItem } from 'reactstrap';
 
-
-
+import {Link} from 'react-router-dom';
 	function RenderDish({dish}){
         if(dish != null)
             return(
@@ -46,19 +45,29 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
 	
 	const Dishdetail=(props)=>{
 				
-				if(props.selectedDish !=null)
+				if(props.dish !=null)
 				return(
 				<div className="container">
 					<div className="row">
-	                  
-	                  <div  className="col-12 col-md-5 m-1">
-	                    <RenderDish dish={props.selectedDish} />
-	                  </div>
-	                  <div className="col-12 col-md-5 m-1" >
-	                    <RenderComments dishComments={props.selectedDish.comments} />
-	                  </div>
+                		<Breadcrumb>
+                  			<BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                  			<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                		</Breadcrumb>
+                		<div className="col-12">
+                  		<h3>{props.dish.name}</h3>
+                  		<hr />
+                		</div>
+              		</div>
+						<div className="row">
+		                  
+		                  <div  className="col-12 col-md-5 m-1">
+		                    <RenderDish dish={props.dish} />
+		                  </div>
+		                  <div className="col-12 col-md-5 m-1" >
+		                    <RenderComments dishComments={props.comments} />
+		                  </div>
 
-	                </div>
+		                </div>
 	             </div>
 				)
 				else return(<div></div>) 
