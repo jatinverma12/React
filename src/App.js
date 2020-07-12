591bf79cd37651d378d3214c927d76f446646e8b
jatinverma12 <jatinverma4621@gmail.com>
2020-07-12 11:55:35 +0530

    1233

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React,{Component} from 'react';
 import Main from './Components/MainComponent';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+//PROVIDER MAKES STORE AVAILABLE TO ALL THE COMPONENTS OF REACT APPLICATION
+import {ConfigureStore} from './Redux/configureStore';
+const store=ConfigureStore();
 
 class App extends Component {
+
+
 	render(){
+		
 		return(
+		<Provider store={store} >
 			<BrowserRouter>
 			<div>
 				<Main />
 			</div>
 			</BrowserRouter>
-
+		</Provider>
 			);
+
 	}
 }
 export default App;
